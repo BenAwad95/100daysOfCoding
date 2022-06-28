b3e0522a9080bf8c7e2e81083590129b887840de
abdullah_awad <abdullah.alashafi@gmail.com>
2022-06-28 13:58:31 +0300

    Very nice and simaple todo app
    you can improve it more and also you use it with text file to store data or andvince step with database like sqlite3, thanks

--- todo_list_by_OOP/code.py
+++ todo_list_by_OOP/code.py
@@ -3,17 +3,58 @@ from datetime import date
 import datetime
 
 class ToDo:
-    def __init__(self, filename='todo.txt'):
-        self.filename = filename
+    def __init__(self):
         self.todo_list = {}
+        self.id = 1
     
-    def add_task(self, task_des: str, date: date=date.today()):
+    def give_id(self):
+        id, self.id = self.id, self.id+1
+        return id
+    
+    def add_task(self, task: str, date: date=date.today()):
         # add the task to the list
         if self.todo_list.get(date):
-            self.todo_list[date] += [task_des]
+            self.todo_list[date][self.give_id()] = {
+                        'task': task,
+                        'is_done': False
+                    }
         else:
-            self.todo_list[date] = [task_des]
+            self.todo_list[date] = {
+                    self.give_id() : {
+                        'task': task,
+                        'is_done': False
+                    }
+                }
     
+    def complete_task(self, date, id):
+        if self.todo_list.get(date):
+            day_tasks_list = self.todo_list[date]
+            if day_tasks_list.get(id):
+                day_tasks_list[id]['is_done'] = True
+            else:
+                return 'Invalid Id'
+        else:
+            return 'Invalid date'
+        return 'Good Job'
+    
+    def remove_day_task(self, date):
+        if self.todo_list.get(date):
+            self.todo_list.pop(date)
+        else:
+            return 'Not Found'
+        return 'Deleted day!'
+
+    def remove_task(self, date, id):
+        if self.todo_list.get(date):
+            day_tasks_list = self.todo_list[date]
+            if day_tasks_list.get(id):
+                day_tasks_list.pop(id)
+            else:
+                return 'Invalid Id'
+        else:
+            return 'Invalid date'
+        return 'Deleted task!'
+
     def show_list(self):
         dates = self.todo_list.keys()
         if not dates:
@@ -21,6 +62,7 @@ class ToDo:
             return
         for date in dates:
             print('Date:', date)
-            for index, task in enumerate(self.todo_list[date], start=1):
-                print(f"{index}: {task}")
+            for key, value in self.todo_list[date].items():
+                state = 'Complete' if value['is_done'] else 'Not complete'
+                print(f"    Id: {key} - {value['task']} - {state}")
         print('End')
